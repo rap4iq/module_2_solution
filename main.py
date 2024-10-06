e7cb6bf4a53c1a4a52785fd0962ec3d5ceed335b
rap4iq <almatykz2004@gmail.com>
2024-10-06 23:21:42 +0600

    init

---
+++ main.py
@@ -0,0 +1,87 @@
+# def say_hello(): print('halo')
+#
+# message = say_hello
+# message()
+
+
+# def sum(a, b): return a+b
+# def multiply(a, b): return a*b
+# def minus(a, b): return a-b
+#
+# def select(choice):
+#     if choice == 1:
+#         return sum
+#     elif choice == 2:
+#         return multiply
+#     elif choice == 3:
+#         return minus
+#
+# operation = select(2)
+# print(operation(6,2))
+
+
+
+
+
+#LOST CARD
+# n = int(input())
+# m = []
+# s = 0
+# for i in range(1, n+1):
+#     a = input()
+#     if a == '':
+#         m.append(0)
+#     else:
+#         m.append(int(a))
+#     s += i
+#
+# print(s - sum(m))
+
+
+
+#SUM OF CUBE
+# a = int(input())
+# b = 0
+# for i in range(1, a+1):
+#     b += i**3
+#
+# print(b)
+
+#sum of factorial
+# a = int(input())
+# b= 0
+# f = 1
+# for i in range(1, a+1):
+#     f *= i
+#     b += f
+# print(b)
+
+
+#sum of zero
+# a = int(input())
+# zero = 0
+# for i in range(1, a+1):
+#     b = int(input())
+#     if b == 0:
+#         zero += 1
+#
+# print(zero)
+
+# a = int(input())
+# for i in range(1, a+1):
+#     for  j in range(1, i+1):
+#         print(j, end='')
+#     print()
+#
+# n = int(input())
+# total = 0
+# for i in range(1,n+1):
+#     number = int(input())
+#     total += number
+# print(total)
+
+# n = input()
+# t = 0
+# for i in n:
+#     t += int(i)
+# print(t)
